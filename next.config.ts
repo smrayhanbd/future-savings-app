c1a270e7b195e68ec58477d06ea29000cd730bf3
Rayhan Rabby <rayhan.rahman@l8r6.onmicrosoft.com>
2026-07-10 02:33:23 +0600

    Clean up next config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // This tells Next.js to ignore ESLint warnings during deployment
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // This tells Next.js to ignore minor TypeScript type errors during deployment
     ignoreBuildErrors: true,
